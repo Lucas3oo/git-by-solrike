16a0c84952a22a4297fc92d563d0a2753c484c21
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-12-07 19:33:15 +0100

    formating

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,18 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand('git-by-solrike.discardChanges', () => {
-    // the git command must take the current selected file
-    vscode.commands.executeCommand('git.clean');
-  }));
-  context.subscriptions.push(vscode.commands.registerCommand('git-by-solrike.openChanges', () => {
-    // the git command must take the current selected file
-    vscode.commands.executeCommand('git.openChange');
-  }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("git-by-solrike.discardChanges", () => {
+      // the git command must take the current selected file
+      vscode.commands.executeCommand("git.clean")
+    })
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand("git-by-solrike.openChanges", () => {
+      // the git command must take the current selected file
+      vscode.commands.executeCommand("git.openChange")
+    })
+  )
 }
 
-export function deactivate() { }
+export function deactivate() {}
