74895efa2e11863dfdb1a007bc56ade2fa03d80c
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-12-09 19:59:53 +0100

    adding compare with branch command

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,32 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand('git-by-solrike.discardChanges', () => {
-    // the git command must take the current selected file
-    vscode.commands.executeCommand('git.clean');
-  }));
-  context.subscriptions.push(vscode.commands.registerCommand('git-by-solrike.openChanges', () => {
-    // the git command must take the current selected file
-    vscode.commands.executeCommand('git.openChange');
-  }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("git-by-solrike.discardChanges", () => {
+      // the git command must take the current selected file
+      vscode.commands.executeCommand("git.clean")
+    })
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand("git-by-solrike.openChanges", () => {
+      // the git command must take the current selected file
+      vscode.commands.executeCommand("git.openChange")
+    })
+  )
+
+  const gitlensExt = vscode.extensions.getExtension("eamodio.gitlens")
+
+  if (gitlensExt?.isActive) {
+    vscode.commands.executeCommand("setContext", "gitlensExtIsActive", true)
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        "git-by-solrike.openChangesWithBranch",
+        () => {
+          vscode.commands.executeCommand("gitlens.diffWithRevisionFrom")
+        }
+      )
+    )
+  }
 }
 
-export function deactivate() { }
+export function deactivate() {}
