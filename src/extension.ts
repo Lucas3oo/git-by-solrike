706b19bd94f698bb57ab7c46586b4af8cb22cec2
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-12-10 17:46:25 +0100

    prepare for new command

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,37 @@
-import * as vscode from "vscode"
+import * as vscode from 'vscode'
+import { API, GitExtension, Repository } from './git'
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("git-by-solrike.discardChanges", () => {
+    vscode.commands.registerCommand('git-by-solrike.discardChanges', () => {
       // the git command must take the current selected file
-      vscode.commands.executeCommand("git.clean")
+      vscode.commands.executeCommand('git.clean')
     })
   )
   context.subscriptions.push(
-    vscode.commands.registerCommand("git-by-solrike.openChanges", () => {
+    vscode.commands.registerCommand('git-by-solrike.openChanges', () => {
       // the git command must take the current selected file
-      vscode.commands.executeCommand("git.openChange")
+      vscode.commands.executeCommand('git.openChange')
     })
   )
 
-  const gitlensExt = vscode.extensions.getExtension("eamodio.gitlens")
+  const gitExtension: GitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports
+  const gitApi: API = gitExtension.getAPI(1)
 
-  if (gitlensExt?.isActive) {
-    vscode.commands.executeCommand("setContext", "gitLensExtIsActive", true)
-    context.subscriptions.push(
-      vscode.commands.registerCommand(
-        "git-by-solrike.openChangesWithBranchOrTag",
-        () => {
-          vscode.commands.executeCommand("gitlens.diffWithRevisionFrom")
-        }
-      )
-    )
-  }
+  context.subscriptions.push(
+    vscode.commands.registerCommand('git-by-solrike.openChangesWithBranch', (uri: vscode.Uri) => {
+      let ref = getDefaultBranch(gitApi, uri)
+      const gitUri = gitApi.toGitUri(uri, ref)
+      const filePath = uri.path.replace(vscode.workspace.rootPath! + '/', '')
+      vscode.commands.executeCommand('vscode.diff', gitUri, uri, filePath + ' compare with "' + ref + '"')
+    })
+  )
 }
 
 export function deactivate() {}
+
+function getDefaultBranch(gitApi: API, uri: vscode.Uri): string {
+  const repo = gitApi.getRepository(uri)
+  const defaultBranch = repo?.state.HEAD?.name || 'main'
+  return defaultBranch
+}
