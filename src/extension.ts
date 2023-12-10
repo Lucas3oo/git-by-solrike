b225a3319877aefe81a7e3455fcbcf6b9dd331c6
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-12-10 14:54:49 +0100

    prepare for new command

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
-import * as vscode from "vscode"
-//import { API, GitExtension, Repository } from "./git"
+import * as vscode from 'vscode'
+import { API, GitExtension, Repository } from './git'
 
 // commands: https://github.com/microsoft/vscode/blob/ce46f445c0f1c3992b78a0e401c6125f1aac7bf9/extensions/git/src/commands.ts
 // search for e.g. @command('git.branchFrom'
@@ -7,83 +7,41 @@ import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("git-by-solrike.discardChanges", () => {
+    vscode.commands.registerCommand('git-by-solrike.discardChanges', () => {
       // the git command must take the current selected file
-      vscode.commands.executeCommand("git.clean")
+      vscode.commands.executeCommand('git.clean')
     })
   )
   context.subscriptions.push(
-    vscode.commands.registerCommand("git-by-solrike.openChanges", () => {
+    vscode.commands.registerCommand('git-by-solrike.openChanges', () => {
       // the git command must take the current selected file
-      vscode.commands.executeCommand("git.openChange")
+      vscode.commands.executeCommand('git.openChange')
     })
   )
-  const gitlensExt = vscode.extensions.getExtension("eamodio.gitlens")
+  // const gitlensExt = vscode.extensions.getExtension('eamodio.gitlens')
 
-  if (gitlensExt?.isActive) {
-    vscode.commands.executeCommand("setContext", "gitlensExtIsActive", true)
-    context.subscriptions.push(
-      vscode.commands.registerCommand(
-        "git-by-solrike.openChangesWithBranch",
-        () => {
-          vscode.commands.executeCommand("gitlens.diffWithRevisionFrom")
-        }
-      )
-    )
-  }
-
-  // context.subscriptions.push(
-  //   vscode.commands.registerCommand("git-by-solrike.diff", () => {
-  //     vscode.commands.executeCommand(
-  //       "vscode.diff",
-  //       vscode.Uri.file(
-  //         "/Users/github-repos/lapce-eclipse-theme/README.md"
-  //       ),
-  //       vscode.Uri.file(
-  //         "/Users//github-repos/lapce-eclipse-theme/volt.toml"
-  //       ),
-  //       "smurf"
-  //     )
-  //   })
-  // )
-  // context.subscriptions.push(
-  //   vscode.commands.registerCommand(
-  //     "git-by-solrike.mycommand",
-  //     (uri: vscode.Uri) => {
-  //       console.log(uri.fsPath)
-  //     }
+  // if (gitlensExt?.isActive) {
+  //   vscode.commands.executeCommand('setContext', 'gitlensExtIsActive', true)
+  //   context.subscriptions.push(
+  //     vscode.commands.registerCommand('git-by-solrike.openChangesWithBranch', () => {
+  //       vscode.commands.executeCommand('gitlens.diffWithRevisionFrom')
+  //     })
   //   )
-  // )
+  // }
 
-  // const gitExtension: GitExtension =
-  //   vscode.extensions.getExtension<GitExtension>("vscode.git").exports
-  // const git: API = gitExtension.getAPI(1)
-
-  // const repo: Repository = git.repositories[0]
+  const gitExtension: GitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports
+  const gitApi: API = gitExtension.getAPI(1)
+  context.subscriptions.push(
+    vscode.commands.registerCommand('git-by-solrike.openChangesWithBranch', (uri: vscode.Uri) => {
+      const gitUri = gitApi.toGitUri(uri, 'main')
+      console.log()
+      vscode.commands.executeCommand('vscode.diff', gitUri, uri, 'smurf')
+    })
+  )
 }
 
 export function deactivate() {}
 
-// some
-
-// function diff ( leftPath, rightPath, commit? ) {
-
-//   const leftUri = vscode.Uri.file ( leftPath ),
-//         rightUri = vscode.Uri.file ( rightPath ),
-//         date = commit && Utils.commit.parse.date ( commit ),
-//         hash = commit && Utils.commit.parse.hash ( commit ),
-//         message = commit && Utils.commit.parse.message ( commit ),
-//         title = commit ? [message, date, hash].join ( ' - ' ) : 'File Diff';
-
-//   return vscode.commands.executeCommand ( 'vscode.diff', leftUri, rightUri, title );
-
-// }
-
 // https://github.com/fabiospampinato/vscode-git-history/blob/master/src/utils.ts
 
 // https://github.com/kappariver/git-open-diff/
-
-// {
-//   "key": "",
-//   "command": "gitlens.diffWithRevisionFrom"
-// }
