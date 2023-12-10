7aaf4b6c96cc0b41b818fd6c2824363dd754206d
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-12-10 17:44:37 +0100

    prepare for new command

--- src/extension.ts
+++ src/extension.ts
@@ -31,17 +31,21 @@ export function activate(context: vscode.ExtensionContext) {
 
   const gitExtension: GitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports
   const gitApi: API = gitExtension.getAPI(1)
+
   context.subscriptions.push(
     vscode.commands.registerCommand('git-by-solrike.openChangesWithBranch', (uri: vscode.Uri) => {
-      const gitUri = gitApi.toGitUri(uri, 'main')
-      console.log()
-      vscode.commands.executeCommand('vscode.diff', gitUri, uri, 'smurf')
+      let ref = getDefaultBranch(gitApi, uri)
+      const gitUri = gitApi.toGitUri(uri, ref)
+      const filePath = uri.path.replace(vscode.workspace.rootPath! + '/', '')
+      vscode.commands.executeCommand('vscode.diff', gitUri, uri, filePath + ' compare with "' + ref + '"')
     })
   )
 }
 
 export function deactivate() {}
 
-// https://github.com/fabiospampinato/vscode-git-history/blob/master/src/utils.ts
-
-// https://github.com/kappariver/git-open-diff/
+function getDefaultBranch(gitApi: API, uri: vscode.Uri): string {
+  const repo = gitApi.getRepository(uri)
+  const defaultBranch = repo?.state.HEAD?.name || 'main'
+  return defaultBranch
+}
